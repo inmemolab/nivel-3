b1dcd57b04ac3dd4fc8cc76656fc05d3eb5cc21b
InMemoLab <59032451+inmemolab@users.noreply.github.com>
2022-06-01 23:17:24 -0500

    fix: 🐛 some fix

--- build/server.js
+++ build/server.js
@@ -10,12 +10,16 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const routes_1 = __importDefault(require("./routes"));
 // montamos app/express
 const app = (0, express_1.default)();
+// set port
+const port = process.env.PORT || 3000;
 // use bodyParser
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
+// show
+app.get("/", (req, res) => res.send("Listos!!!"));
 // routes api and app
 app.use("/api/", routes_1.default);
 // app
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("App iniciada");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 // importamos express
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 // routes
 import apiRouter from "./routes";
 // montamos app/express
 const app = express();
+// set port
+const port = process.env.PORT || 3000;
 // use bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// show
+app.get("/", (req: Request, res: Response) => res.send("Listos!!!"));
 // routes api and app
 app.use("/api/", apiRouter);
 // app
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("App iniciada");
 });
